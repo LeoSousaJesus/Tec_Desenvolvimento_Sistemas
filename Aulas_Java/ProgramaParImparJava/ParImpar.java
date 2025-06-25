ce30eab41767cfa7ac7b17b620b5185a70fd8bfb
LeoSousaJesus <leandrosousajesus@gmail.com>
2025-06-24 23:58:13 -0300

    Adicionando Programa Atividade 19

--- Aulas_Java/ProgramaParImparJava/ParImpar.java
+++ Aulas_Java/ProgramaParImparJava/ParImpar.java
@@ -1,13 +1,20 @@
 // Programa para verificar se um número é par ou ímpar em Java
 
+
 public class ParImpar {
     public static void main(String[] args) {
-        int numero = 10; // Exemplo de número para verificar
-
-        if (numero % 2 == 0) {
-            System.out.println(numero + " é par.");
+        java.util.Scanner tec = new java.util.Scanner(System.in);
+        
+        System.out.println("Digite um numero para saber se ele e impar ou par: ");
+        int escolha = tec.nextInt();
+        
+        // Verifica se o número é par ou ímpar
+        if (escolha % 2 == 0) {
+            System.out.println("O número " + escolha + " é par.");
         } else {
-            System.out.println(numero + " é ímpar.");
+            System.out.println("O número " + escolha + " é ímpar.");
         }
+        
+        tec.close();
     }
 }
---
+++ Programas_Portugol/Atividade19.por
@@ -0,0 +1,10 @@
+// Crie um algoritmo com três variáveis num1, num2 e num3 com os respectivos valores 100, 5 e 2. Calcule e apresente a seguinte expressão (num1 + num2) * num3.
+
+programa {
+  funcao inicio() {
+    inteiro num1 = 100, num2 = 5, num3 = 2
+    inteiro resultado = (num1 + num2) * num3
+    
+    escreva("(", num1, " + ", num2, ") x ", num3, " = ", resultado)
+  }
+}
