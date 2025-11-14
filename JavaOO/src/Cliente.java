c3df1d684236c51a8672c0167cd3bcf90fd1ffc1
LeoSousaJesus <leandrosousajesus@gmail.com>
2025-11-14 16:02:10 -0300

    Adicionando classe Cliente - Exemplo de OO

--- JavaOO/src/Cliente.java
+++ JavaOO/src/Cliente.java
@@ -1,7 +1,67 @@
 public class Cliente {
-    private int id;
     private String nome;
     private String email;
     private String telefone;
-    private double salario;
+    private double salarioBruto;
+
+    public Cliente(String nome, String email, String telefone, double salarioBruto) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.salarioBruto = salarioBruto;
+    }
+
+    public Cliente() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public double getSalarioBruto() {
+        return salarioBruto;
+    }
+
+    public void setSalarioBruto(double salarioBruto) {
+        this.salarioBruto = salarioBruto;
+    }
+    @Override
+    public String toString() {
+        return """
+                
+        ================================
+            DADOS DE FUNCIONÁRIO
+        ================================
+           Nome = %s
+           Email = %s
+           Telefone = %s
+           Salário = R$ %,.2f
+           ================================
+           """.formatted(
+                    this.getNome(),
+                    this.getEmail(),
+                    this.getTelefone(),
+                    this.getSalarioBruto()
+        );
+    }
 }
--- JavaOO/src/Main.java
+++ JavaOO/src/Main.java
@@ -3,5 +3,17 @@
 public class Main {
     public static void main(String[] args) {
         Cliente cliente1 = new Cliente();
+        cliente1.setNome("Leo");
+        cliente1.setEmail("leo@email.com");
+        cliente1.setTelefone("6193457894");
+        cliente1.setSalarioBruto(8500);
+        System.out.println("nome: "+cliente1.getNome());
+
+        Cliente cliente2 = new Cliente();
+        cliente1.setNome("Dani");
+        cliente1.setEmail("dani@email.com");
+        cliente1.setTelefone("6198547896");
+        cliente1.setSalarioBruto(10500);
+        System.out.println("nome: "+cliente1.getNome());
     }
 }
